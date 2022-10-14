8d70251d78dba34cbad7db79dda0cd43f4996c38
KarimoSP <KarimSP_98@outlook.com>
2022-10-14 15:19:35 -0500

    Updat&Delete
    Implementacion del Update y del Delete

--- Middleware/routes/donors.js
+++ Middleware/routes/donors.js
@@ -7,6 +7,8 @@ let donorsController = require('../controllers/donorsController')
 
 router.get('/', donorsController.get_donors)
 router.get('/donor/:id', donorsController.get_donor)
+router.delete('/donor/:id', donorsController.del_donor)
+router.update('/donor/:id', donorsController.upd_donor)
 router.post('/donor', donorsController.add_donor)
 
 module.exports = router

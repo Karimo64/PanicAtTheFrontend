10b59add55580730793a2916f7a7613c0d42575a
JesusGH123 <a01423730@itesm.mx>
2022-11-02 17:15:36 -0600

    API v1.1

--- Middleware/routes/donations.js
+++ Middleware/routes/donations.js
@@ -6,7 +6,6 @@ let router = express.Router()
 let donationsController = require('../controllers/donationsController')
 
 router.get('/', donationsController.get_donation)
-//router.get('/donor/:id', donationsController.ge)
 router.post('/', donationsController.add_donation)
 
 module.exports = router
--- Middleware/routes/mails.js
+++ Middleware/routes/mails.js
@@ -5,7 +5,6 @@ let router = express.Router()
 
 let mailsController = require('../controllers/mailsController')
 
-//router.get('/', mailsController.get_mails)
 router.get('/donor/:id', mailsController.get_mails)
 router.post('/', mailsController.add_mail)
 

c2493d6b3bfb436b97733f288d073a430abfe971
JesusGH123 <a01423730@itesm.mx>
2022-11-02 18:10:31 -0600

    Frontend structure

--- Frontend/BAMX/src/App.vue
+++ Frontend/BAMX/src/App.vue
@@ -1,47 +1,36 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <h1>Banco de alimentos Morelos</h1>
+    <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <link rel="icon" href="/favicon.ico">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Banco de alimentos</title>
+  </head>
+  <body>
+    
+    <table style="width:100%">
+      <tr>
+        <th>Id</th>
+        <th>Nombre</th>
+        <th>Localizaci&oacute;n</th>
+        <th>Organizaci&oacute;n/Gremio</th>
+        <th>Tipo de aliado</th>
+        <th>Tel&eacute;fono</th>
+        <th>Correo electr&oacute;nico</th>
+      </tr>
+      <tr>
+        <td>{{donor}}</td>
+      </tr>
+    </table>
+  </body>
+  </html>
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
